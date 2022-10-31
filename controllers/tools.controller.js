acc2849a4140218420ade1088a916f3e9672c502
mdasif8393 <md.asif.8393@gmail.com>
2022-10-31 22:56:07 +0600

    complete

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,16 +1,49 @@
-module.exports.getAllTools = (req, res, next) => {
-    // const {ip, query, params, body, headers} = req;
-    // console.log(ip, query, params, body, headers);
+let tools = [
+    { id: 1, name: "Hammer" },
+    { id: 2, name: "Hammer2" },
+    { id: 3, name: "Hammer3" }
+];
 
-    // res.download(__dirname + "/tools.controller.js")
-    // res.json({"name": "abc"})
-    res.send("Data found")
+module.exports.getAllTools = (req, res, next) => {
+    const { limit, page } = req.query;
+    res.json(tools.slice(0, limit))
 }
 
 module.exports.saveATool = (req, res, next) => {
-    res.send("Tool added")
+
+    tools.push(req.body)
+    res.send(tools)
 }
 
 module.exports.getToolDetail = (req, res, next) => {
-    res.send("Tool detail found")
+    const { id } = req.params;
+    // const filter ={_id: id}
+    const foundTool = tools.find(tool => tool.id === Number(id));
+    res.status(200).send({
+        success: true,
+        message: "Success",
+        data: foundTool
+    })
+    res.status(500).send({
+        success: false,
+        error: "Internal Server Error"
+    })
+}
+
+module.exports.updateTool = (req, res, next) => {
+    // const newData = req.body;
+    const { id } = req.params;
+    // const filter = { _id: id };
+    const newData = tools.find(tool => tool.id === Number(id));
+    newData.id = id;
+    newData.name = req.body.name;
+    res.send(newData);
+}
+
+module.exports.deleteTool = (req, res) => {
+    const {id} = req.params;
+    const filter = {_id: id};
+    tools = tools.filter (tool => tool.id !== Number(id));
+    res.send(tools)
+
 }
--- routes/v1/tools.route.js
+++ routes/v1/tools.route.js
@@ -13,40 +13,44 @@ const router = express.Router();
 // })
 
 router
-    .route("/")
-     /**
-   * @api {get} /tools All tools
-   * @apiDescription Get all the tools
-   * @apiPermission admin
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiParam  {Number{1-}}         [page=1]     List page
-   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
-   *
-   * @apiSuccess {Object[]} all the tools.
-   *
-   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
-   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
-   */
-    .get(toolsControllers.getAllTools)
-     /**
-   * @api {post} /tools save a tool
-   * @apiDescription Get all the tools
-   * @apiPermission admin
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiParam  {Number{1-}}         [page=1]     List page
-   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
-   *
-   * @apiSuccess {Object[]} all the tools.
-   *
-   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
-   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
-   */
-    .post(toolsControllers.saveATool)
+  .route("/")
+  /**
+* @api {get} /tools All tools
+* @apiDescription Get all the tools
+* @apiPermission admin
+*
+* @apiHeader {String} Authorization   User's access token
+*
+* @apiParam  {Number{1-}}         [page=1]     List page
+* @apiParam  {Number{1-100}}      [limit=10]  Users per page
+*
+* @apiSuccess {Object[]} all the tools.
+*
+* @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+* @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+*/
+  .get(toolsControllers.getAllTools)
+  /**
+* @api {post} /tools save a tool
+* @apiDescription Get all the tools
+* @apiPermission admin
+*
+* @apiHeader {String} Authorization   User's access token
+*
+* @apiParam  {Number{1-}}         [page=1]     List page
+* @apiParam  {Number{1-100}}      [limit=10]  Users per page
+*
+* @apiSuccess {Object[]} all the tools.
+*
+* @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+* @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+*/
+  .post(toolsControllers.saveATool)
 
-router.route("/:id").get(viewCount, limiter, toolsControllers.getToolDetail);
+router
+  .route("/:id")
+  .get(viewCount, limiter, toolsControllers.getToolDetail)
+  .patch(toolsControllers.updateTool)
+  .delete(toolsControllers.deleteTool)
 
 module.exports = router;
